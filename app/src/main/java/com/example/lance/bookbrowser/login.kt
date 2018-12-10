2ae206272cbf1ccf0fffcf45bed65e224e8f7822
LanceDCantu <Lance@nevada.unr.edu>
2018-12-09 17:28:02 -0800

    Added Navigation to the first page. Now you must use login: firstui and password to get into the application.
    If you want to make things easier on yourself changes the intent filters
    in app/manifest/AndroidManifest.xml in the login acitivity to the main
    activity so you don't have to login in every run.

--- app/src/main/java/com/example/lance/bookbrowser/login.kt
+++ app/src/main/java/com/example/lance/bookbrowser/login.kt
@@ -1,5 +1,6 @@
 package com.example.lance.bookbrowser
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -13,10 +14,21 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        var status: String;
+
         login_btn.setOnClickListener {
-            var status=if(username_et.text.toString().equals("firstui")
-                && password_et.text.toString().equals("password")) "Logged In Successfully" else "Login failed"
-            Toast.makeText(this,status, Toast.LENGTH_SHORT).show()
+            if(username_et.text.toString().equals("firstui")
+                && password_et.text.toString().equals("password"))
+            {
+               status = "Logged In Successfully"
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                status = "Login failed"
+            }
+            Toast.makeText(this, status, Toast.LENGTH_SHORT).show()
         }
     }
 }
