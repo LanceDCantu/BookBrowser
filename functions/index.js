87044be85428f85d897f0a6bd7fd80d617f59393
Emily Gentry <emilyruthgentry@gmail.com>
2019-03-05 09:52:49 -0800

    Need to learn promises

--- functions/index.js
+++ functions/index.js
@@ -1,78 +1,46 @@
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-// import the module
 var request = require('request');
-
-var xml2js = require('xml2js');
+var parseString = require('xml2js').parseString;
 const functions = require('firebase-functions');
-
-
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.bookInfo = functions.https.onRequest((req, res) => {
-    const isbn = req.query.isbn;
-    return admin.database().ref('/books').push({isbn: isbn}).then((snapshot) => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        return res.redirect(303, snapshot.ref.toString());
-      });
-});
+function parseXml(data)
+{
+    var result = {};
+    parseString(data, (err, xml) => {
+        if (err) throw err;
+        console.dir('in parse:', JSON.stringify(xml));
+        result.bookimageurl = 'https://nowhere.com';
+        result.description = 'description';
+        result.iframe = '<iframe/>';
+    });
 
-exports.parseXML = (req, res) => {
-    //Convert the request to a Buffer and a string
-    //Use whichever one is accepted by your XML parser
-    let data = req.rawBody;
-    let xmlData = data.toString();
+    return result;
 
-    const parseString = require('xml2js').parseString
 }
 
-exports.bookLookUp = functions.database.ref('/books/{pushId}/isbn')
-    .onCreate((snapshot, context) => {
-      // Grab the current value of what was written to the Realtime Database.
-      const isbn = snapshot.val();
-      console.log('bookLookUp', context.params.pushId, isbn);
-      const goodreadsUrl = functions.config().goodreads.url;
-      const goodreadsKey = functions.config().goodreads.key;
-      const url = goodreadsUrl + '/book/isbn/' + isbn +'?key=' + goodreadsKey;
-      console.log('url is ' + url);
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      // make the request
-      request(url, (error, response, body) => {
-          if (!error && response.statusCode === 200) {
+exports.bookInfo = functions.https.onRequest((req, res) => {
+    const isbn = req.query.isbn;
+    console.log('bookInfo', isbn);
+    const goodreadsUrl = functions.config().goodreads.url;
+    const goodreadsKey = functions.config().goodreads.key;
+    const url = goodreadsUrl + '/book/isbn/' + isbn +'?key=' + goodreadsKey;
+    console.log('url is ' + url);
+    var result = {'alpha':'beta'};
+    request(url, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
               console.log('recieved ' + body);
-              var extractedData = "";
-              var parser = new xml2js.Parser();
-              parser.parseString(body, function(err, result){
-                if(err)
-                {
-                    console.error(err);
-                    return;
-                }
-                //Extract the value from the data element
-                extractedData = result;
-                console.log(extractedData);
-              });
+              result = parseXml(body);
+              console.dir('result:', JSON.stringify(result));
+        } else {
+              console.log('went wrong!!', error);
+        }
 
-
-              return snapshot.ref.parent.child('bookInfo').set(extractedData);
-              //return snapshot.ref.parent.child('bookInfo').set(body);
-              //snapshot.ref.parent.child('bookInfo').set(body);
-          }
-          //return snapshot.ref.parent.child('status-code').set(response.statusCode);
-          else {
-            console.log('status code = ' + response.statusCode);
-          }
-      });
-      return snapshot.ref.parent;
-    });
+        });
+        res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(result);
+});
 
 
 
